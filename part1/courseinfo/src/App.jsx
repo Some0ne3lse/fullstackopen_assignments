71d27571d6dfe55f58586f096bd67df83111dacf
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-03-24 16:06:46 +0000

    Step 5 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Part = (props) => {
@@ -13,9 +13,18 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+      <Part
+        part={props.name.parts[0].name}
+        exercise={props.name.parts[0].exercises}
+      />
+      <Part
+        part={props.name.parts[1].name}
+        exercise={props.name.parts[1].exercises}
+      />
+      <Part
+        part={props.name.parts[2].name}
+        exercise={props.name.parts[2].exercises}
+      />
     </>
   );
 };
@@ -24,35 +33,37 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
+      {props.name.parts[0].exercises +
+        props.name.parts[1].exercises +
+        props.name.parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content name={course} />
+      <Total name={course} />
     </>
   );
 };
